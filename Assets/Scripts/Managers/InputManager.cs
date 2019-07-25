d43510e7f60cb060707331d8b662c95380647476
Meriç Balıkçı <meric.balikci.mb@gmail.com>
2019-07-25 14:12:53 +0300

    InputManager.cs and LevelManager.cs have been added.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WreckingBall
+{
+    /// <summary>
+    /// This script controls input of the game.
+    /// </summary>
+    public static class InputManager
+    {
+        /// <summary>
+        /// This method controls whether the screen is tapted or not.
+        /// </summary>
+        /// <returns>True or false</returns>
+        public static bool IsScreenTapted()
+        {
+            return Input.GetMouseButtonDown(0);
+        }
+    }
+}
